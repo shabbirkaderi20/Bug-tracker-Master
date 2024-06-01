ef3e954be603c442eb82e4b263095b21e4b4ca3f
shabbirkaderi20 <82316061+shabbirkaderi20@users.noreply.github.com>
2024-06-02 04:24:15 +0530

    Comments for improvement for the functionalities

--- user-service/src/main/java/com/bugtracker/user/controller/UserController.java
+++ user-service/src/main/java/com/bugtracker/user/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/* New controller changes for enhancement comments */
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -33,6 +34,7 @@ public class UserController {
         return userService.userLogin(userName, password, roleId);
     }
 
+    /* Use the OAuth2 for multiple login options */
     @PostMapping("/authenticate")
     public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
@@ -44,24 +46,28 @@ public class UserController {
         return jwtUtil.generateToken(authRequest.getUserName());
     }
 
+    /* Register with only mandatory details also if partial */
     @PostMapping(value= "/registerUserAccount")
     public User registerUser(@RequestBody UserDto userDto) {
 
         return userService.registerUser(userDto);
     }
 
+    /* Implement search functionality for search with name, ID, role */
     @GetMapping(value= "/getUser/{id}")
     public ResponseTemplateVO getUserById(@PathVariable("id") Long userId) {
 
         return userService.getUser(userId);
     }
 
+    /* Update partial data using patch method */
     @PutMapping(value= "/updateUserAccount")
     public User updateUser(@RequestBody UpdateUserDto updateUserDto) {
 
         return userService.updateUser(updateUserDto);
     }
 
+    /* Make more secure by improving de-crypt and en-crypt to unknown algorithm */
     @PutMapping(value= "/changePassword")
     public User changePassword(@RequestParam Long userId, @RequestParam String currentPassword, @RequestParam String newPassword) {
 
@@ -74,35 +80,41 @@ public class UserController {
         return userService.changeEmail(userId, password, email);
     }
 
+    /* Implement search functionality for search with name, ID, role */
     @GetMapping(value= "/getStaffById/{id}")
     public User getStaffById(@PathVariable("id") Long userId) {
 
         return userService.viewStaffById(userId);
     }
 
+    /* Implement search functionality for search with name, ID, role */
     @GetMapping(value= "/getClientById/{id}")
     public User getClientById(@PathVariable("id") Long userId) {
 
         return userService.viewClientById(userId);
     }
 
+    /* Implement search functionality for search with name, ID, role */
     @GetMapping(value= "/getAllClient")
     public List<User> getALlClient() {
 
         return userService.viewAllClient();
     }
 
+    /* Implement search functionality for search with name, ID, role */
     @GetMapping(value= "/getAllStaff")
     public List<User> getALlStaff() {
 
         return userService.viewAllStaff();
     }
 
+    /* Delete by name and id both */
     @DeleteMapping("/deleteClient/{id}")
     public Boolean deleteClientById(@PathVariable("id") Long clientId) {
         return userService.deleteClientById(clientId);
     }
 
+    /* Delete by name and id both */
     @DeleteMapping("/deleteStaff/{id}")
     public Boolean deleteStaffById(@PathVariable("id") Long staffId) {
         return userService.deleteStaffById(staffId);
